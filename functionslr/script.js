c956156e8a7bcfde7645b7110be60a4acc457f67
Your Name <your.email@example.com>
2025-08-07 19:02:19 +0530

    its 4:58:25 , 3hrs remaining

--- functionslr/script.js
+++ functionslr/script.js
@@ -1,2 +1,44 @@
-// 1234efsdfs
-// gmkermgk
+// function 
+// i want ki code likh dun but execute baaad mai karun 
+
+
+// 1st way (// function declaration )
+// function ("kuch bh naam likh lo "){
+//     statement , command 
+// }
+
+
+
+// // 2nd way (// function expression )
+// let name = function(){
+//     kaam likh lo 
+// }
+// is case mai name ki jagha kuch bhi rakh do or woh function ka name ban jiyga 
+
+// let myName = function(){
+//     console.log(" parakram tomar ");
+// }
+// // now call the function 
+// myName() ; 
+
+
+// 3rd way (// fat  arrow function )
+// let myName = () =>{
+//      console.log("my name is  parakram tomar ");
+// }
+// myName() ; // call the functions 
+// topic 1 finish  
+
+
+// now parameters and arguments 
+
+// function printName (name){
+//     console.log ("my name is "+ name  )
+// }
+
+// giver your name and then call fuunctions 
+
+// printName("parakram tomar ")
+// printName (" sarthak mishra ")
+// printName (" anshul mishra ")
+

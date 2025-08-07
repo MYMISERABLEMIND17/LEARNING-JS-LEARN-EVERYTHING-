2e72f420979af0e6e0703218e48bdef20ed1d236
Your Name <your.email@example.com>
2025-08-07 18:21:34 +0530

    Added loops folder

---
+++ loops/script.js
@@ -0,0 +1,28 @@
+// repeat  karne ko loop kahate hain 
+
+
+// for loop 
+// kahan se jana hai -> kahan tak jana hai -> or kaise jana hai
+// we wioll use " for " loop  in this case 
+// 1 --- 30
+
+
+for (let i = 1 ; i<= 1000 ; i++){
+    console.log(i);
+}
+
+
+
+
+
+// while loop 
+// kaha se jana hai -> kavb rukna hai -> kaise jana hai 
+// we will use while loop
+// jab sure na ho 
+
+
+
+
+
+
+

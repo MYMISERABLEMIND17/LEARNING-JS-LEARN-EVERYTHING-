a4f391957f6331988ae376547a26fbd32f3c6c2b
Your Name <your.email@example.com>
2025-08-12 00:30:18 +0530

    add js file

---
+++ dom/script.js
@@ -0,0 +1,11 @@
+// dom manipulation 
+// html se element selecr karna
+// text se element select karna 
+// text badalna 
+// html badalna 
+// css badalna
+// attributes 
+// event listeners
+
+
+

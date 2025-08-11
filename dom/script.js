12c127c3816e838f56e2ca1902b663f23ed8af54
Your Name <your.email@example.com>
2025-08-12 01:24:25 +0530

    its 1:23:03 and i am learning dom

--- dom/script.js
+++ dom/script.js
@@ -7,5 +7,30 @@
 // attributes 
 // event listeners
 
+// let pst = document.getElementById("welcome") // the method to select the element by the id 
+// if you want to select it with the class you can use 
+// let ps = document.getElementsByClassName("first") ;// we can select multiple items by this method 
+// we can select multiple items by this method 
+// console.log(pst);
+// console.dir(ps);
 
+// basically element de do ==
+// 
+// let abcd ==> document.querySelectorAll("element ke naam ")
+// let abcd = document.querySelector("h1") // select all element with h1 tag 
+// console.dir(abcd);
 
+// first manipulation (text and content access )
+
+//let f  = document.querySelector("h1") ;// now go to deop down and see where you have to cahnge html or text 
+// console.dir(f);
+// f.innerText = ("aacha hun") ; 
+
+// dekho ==> innerText , innerContent, innerHTML, ye sab alag alag cheezin hain 
+// innerHTML (change the )main html)
+// let f  = document.querySelector("h1") ;
+// f.innerHTML = "<i>mujhe badal do </i>";
+// console.dir(f);
+
+// attribute manipulation
+// ==> except tag baaki jo bhi cheez tag ke andar hoti hai like href , name , charset 
